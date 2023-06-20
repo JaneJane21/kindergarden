5d3805405ef775e84e26aa88744c32e5577fa67b
JaneJane21 <novocshanova212016@gmail.com>
2023-06-20 11:42:46 +0300

    first commit

---
+++ src/components/ContentBlock.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="header">
+      <h3></h3>
+    </div>
+  <div class="content">
+    <slot></slot>
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+props{
+  title{
+
+  }
+}
+}
+</script>
+
+<style scoped>
+
+</style>
